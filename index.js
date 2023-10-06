c0771721dc8e703a47fe733162d8a0b0c8b3a73d
Adan Nicolas Cabanas Garza <adan.cabanas@gmail.com>
2023-10-06 15:40:58 -0400

    DOMContentHasLoaded event loaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    let textParagraph = document.getElementById("text")
+    textParagraph.textContent = "This is really cool!"
+})
